4f88255c96fdb53bb8a7a0edd2a7a76cc77c70ce
Juan Antonio <1317580+JuantonLab@users.noreply.github.com>
2024-11-17 15:46:05 -0500

    feat: add error boundary demo

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,25 @@ import "./App.css";
 import { Home } from "./pages/Home.page";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ErrorBoundary } from "react-error-boundary";
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Home />
-      <ReactQueryDevtools initialIsOpen={false} />
+      <ErrorBoundary
+        fallbackRender={({ resetErrorBoundary }) => {
+          return (
+            <div>
+              <h1>Hubo un error</h1>
+              <button onClick={() => resetErrorBoundary()}>Try again</button>
+            </div>
+          );
+        }}
+      >
+        <Home />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ErrorBoundary>
     </QueryClientProvider>
   );
 }
